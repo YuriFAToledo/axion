b5a34bdd5cf842272f871e820b09866b818c3f2e
Yuri Toledo <140438205+YuriFAToledo@users.noreply.github.com>
2025-04-11 05:38:59 -0300

    Update ObraDetalhes.js

--- frontend/src/components/ObraDetalhes.js
+++ frontend/src/components/ObraDetalhes.js
@@ -1,7 +1,7 @@
 "use client";
 
 import React from 'react';
-import { ChevronDown } from 'lucide-react';
+// import { ChevronDown } from 'lucide-react';
 
 // const InfoGroup = ({ title, children }) => {
 //   const [isOpen, setIsOpen] = React.useState(true);
